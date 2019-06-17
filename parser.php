0e795e59c6afea483fba89bac0e3a94aaf197988
oiu850714 <oiu850714@gmail.com>
2019-06-17 22:26:59 +0800

    呼叫 js 把 search json param 做 encode

--- parser.php
+++ parser.php
@@ -1,11 +1,19 @@
 <?php
 include __DIR__ . '/vendor/autoload.php';
 
-$download_list = include __DIR__ . '/download_list';
+$download_list = include __DIR__ . '/download_list.php';
 
 use DiDom\Document;
 use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
+
+foreach ($download_list as $download_record) {
+    $download_file_path = $download_record['file_path'];
+    $list_count = $download_record['count'];
+    $query_json = '{"query":[{"field":"series","value":"PLACEHOLDER","attr":"+"}],"domconf":{"zong_level_content":"block","post_query_content":"none","facetsby":"fonds","fonds":"block","yearrange":"none","list_member":"none","list_location":"none","list_organ":"none","query_history_content":"block","sortby":"identifier","pageing":"20","jp_term_dictionary":"none"}}';
+    $query_json = str_replace('PLACEHOLDER', $download_file_path, $query_json);
+    $url_query_string = shell_exec("node js_library.js '$query_json'");
+
 
 $url = '要爬的網頁';
 
@@ -111,4 +119,5 @@ foreach($result_headers as $result_header) {
     file_put_contents("$save_path/$subject_number-$subject.zip", $body);
 
     echo "第三步: guzzle body size: " . strlen($body) . "\n";
+}
 }
