e2d3cb7fb4087f9482ba262fd25feca07d0ec4d0
oiu850714 <oiu850714@gmail.com>
2019-09-21 00:43:22 +0800

    刪除 echo 下載資訊邏輯

--- parser.php
+++ parser.php
@@ -15,9 +15,6 @@ use App\Models\TaiwanGovernorDocument\DownloadList;
     $query_json = str_replace('PLACEHOLDER', $download_file_path, $query_json);
     $url_query_string = shell_exec("node Libraries/js_library.js '$query_json'");
 
-    echo "========= 現在處理到第 {$download_record['id']} 筆檔案路徑 =========\n\n";
-    echo "路徑: $download_file_path\n";
-    echo "數量: $list_count\n\n\n\n\n";
 
     $url = "/index.php?act=Archive/search/$url_query_string";
 
