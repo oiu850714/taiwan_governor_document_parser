ff765d9661aa1210dda5222bf17f331d17d58179
oiu850714 <oiu850714@gmail.com>
2019-09-21 00:39:13 +0800

    變更初始化搜尋要查詢的檔案階層的方式
    拆掉 loop 要搜尋所有檔案階層的邏輯
    改成一次只讀一筆 DB row 的檔案階層並用這筆 row 組出 url

--- parser.php
+++ parser.php
@@ -1,15 +1,16 @@
 <?php
-include __DIR__ . '/vendor/autoload.php';
-
-$download_list = include __DIR__ . '/download_list.php';
+include __DIR__ . '/init/init.php';
 
 use DiDom\Document;
 use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
+use App\Models\TaiwanGovernorDocument\DownloadList;
 
-foreach ($download_list as $download_record) {
-    $download_file_path = $download_record['file_path'];
-    $list_count = $download_record['count'];
+
+    $download_record = DownloadList::first();
+    $download_file_path = $download_record->file_path;
+    $start_index = $download_record->start_index;
+    $list_count = $download_record->count;
     $query_json = '{"query":[{"field":"series","value":"PLACEHOLDER","attr":"+"}],"domconf":{"zong_level_content":"block","post_query_content":"none","facetsby":"fonds","fonds":"block","yearrange":"none","list_member":"none","list_location":"none","list_organ":"none","query_history_content":"block","sortby":"identifier","pageing":"20","jp_term_dictionary":"none"}}';
     $query_json = str_replace('PLACEHOLDER', $download_file_path, $query_json);
     $url_query_string = shell_exec("node js_library.js '$query_json'");
@@ -128,4 +129,3 @@ foreach ($download_list as $download_record) {
         echo "第三步: guzzle body size: " . strlen($body) . "\n";
     }
     }
-}
