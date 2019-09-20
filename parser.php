78789a8c09ac25dc265bfcc59b8a24fb0b70086a
oiu850714 <oiu850714@gmail.com>
2019-09-21 00:42:20 +0800

    變更 include 網站上 js library 的方式

--- parser.php
+++ parser.php
@@ -13,7 +13,7 @@ use App\Models\TaiwanGovernorDocument\DownloadList;
     $list_count = $download_record->count;
     $query_json = '{"query":[{"field":"series","value":"PLACEHOLDER","attr":"+"}],"domconf":{"zong_level_content":"block","post_query_content":"none","facetsby":"fonds","fonds":"block","yearrange":"none","list_member":"none","list_location":"none","list_organ":"none","query_history_content":"block","sortby":"identifier","pageing":"20","jp_term_dictionary":"none"}}';
     $query_json = str_replace('PLACEHOLDER', $download_file_path, $query_json);
-    $url_query_string = shell_exec("node js_library.js '$query_json'");
+    $url_query_string = shell_exec("node Libraries/js_library.js '$query_json'");
 
     echo "========= 現在處理到第 {$download_record['id']} 筆檔案路徑 =========\n\n";
     echo "路徑: $download_file_path\n";
