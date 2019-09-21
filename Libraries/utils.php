4d4a451d28e96abf57266d177babd12dd0b039f7
oiu850714 <oiu850714@gmail.com>
2019-09-21 11:23:57 +0800

    rename var $download_record -> $download_list

--- Libraries/utils.php
+++ Libraries/utils.php
@@ -1,11 +1,11 @@
 <?php
 use App\Models\TaiwanGovernorDocument\DownloadList;
 
-function getArchiveUrl(DownloadList $download_record) {
+function getArchiveUrl(DownloadList $download_list) {
 
-    $download_file_path = $download_record->file_path;
-    $start_index = $download_record->start_index;
-    $list_count = $download_record->count;
+    $download_file_path = $download_list->file_path;
+    $start_index = $download_list->start_index;
+    $list_count = $download_list->count;
     $query_json = '{"query":[{"field":"series","value":"PLACEHOLDER","attr":"+"}],"domconf":{"zong_level_content":"block","post_query_content":"none","facetsby":"fonds","fonds":"block","yearrange":"none","list_member":"none","list_location":"none","list_organ":"none","query_history_content":"block","sortby":"identifier","pageing":"20","jp_term_dictionary":"none"}}';
     $query_json = str_replace('PLACEHOLDER', $download_file_path, $query_json);
     $url_query_string = shell_exec("node " . __DIR__ . "/js_library.js '$query_json'");
