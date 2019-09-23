fc47e2a2b3b06b537563d15b7cd1685d0f72a1fa
oiu850714 <oiu850714@gmail.com>
2019-09-23 11:34:29 +0800

    ErrorSubject add column start_index

--- Models/ErrorSubject.php
+++ Models/ErrorSubject.php
@@ -14,6 +14,7 @@ class ErrorSubject extends Model
         'subject_number', // 典藏號
         'book', // 隸屬冊名
         'file_path', // 檔案階層
+        'start_index', // 方便查詢是從第幾筆開始查的
         'error_message', // 錯誤訊息
     ];
 }
